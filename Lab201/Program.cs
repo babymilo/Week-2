2e28b899e93f157edaaac222ab7755e72db4f8d2
babymilo <peerawit_sf1234@hotmail.com>
2015-08-28 22:33:43 +0700

    Lab201 create student class

--- Lab201/Program.cs
+++ Lab201/Program.cs
@@ -1,15 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Lab201;
 
 
-namespace Lab201
-    
+namespace Lab20_1
+
 {
-	class MainClass
+    class MainClass
 	{
 		public static void Main (string[] args)
 		{
